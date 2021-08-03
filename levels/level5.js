8a13dde65ab7666824a1c73508dd70113312cc9d
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2021-08-03 17:08:51 +0200

    normal solution

--- levels/level5.js
+++ levels/level5.js
@@ -31,7 +31,7 @@ export const LEVEL_5 = {
 
 solution : [
 
-    new Command(Command.repeat_until, new Command(Command.reached_end), [
+  /*  new Command(Command.repeat_until, new Command(Command.reached_end), [
 
         new Command(Command.if_do_else, new Command(Command.and, new Command(Command.is_color, Color.blue), [new Command(Command.is_tile_ahead, "Splat")]), [
             new Command(Command.walk),
@@ -44,6 +44,26 @@ solution : [
         ]),
 
         new Command(Command.walk)
+    ]) */
+
+    new Command(Command.repeat_until, new Command(Command.is_tile_ahead,"Wall"), [
+        new Command(Command.walk)
+    ]), 
+    new Command(Command.turn, Turn.left),
+    new Command(Command.repeat_until, new Command(Command.is_tile_ahead,"Wall"), [
+        new Command(Command.walk)
+    ]),
+    new Command(Command.turn, Turn.back),
+    new Command(Command.repeat_until, new Command(Command.is_tile_current, "Mixer") , [
+        new Command(Command.walk)
+    ]),
+    new Command(Command.turn, Turn.back),
+    new Command(Command.repeat_until, new Command(Command.reached_end), [
+        new Command(Command.if_do_else, new Command(Command.is_tile_ahead,"Wall"), [
+            new Command(Command.turn, Turn.left)
+        ], [
+            new Command(Command.walk)
+        ])
     ])
 ]
 
