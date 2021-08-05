c19f1be7d6465d0a8d088bd6706d51245ea62beb
Conrad <u04564210@tuks.co.za>
2021-08-05 12:32:16 +0200

    Acid is neutralised by grey

--- models/color.js
+++ models/color.js
@@ -13,19 +13,37 @@ export class Color {
     static acid = 9;
 
     // static mix(color1, color2) {
+    //     if ((color1 == 9 || color2 == 9)) {
+    //         if (color1 == 0 || color2 == 0) {
+    //             return 0;
+    //         }
+    //         return 9;
+    //     }
     //     return color1 | color2;
     // }
 
     static mix(color1, color2) {
         let mixCol = color1 + color2;
         if (color1 == 0 || color2 == 0) {
+            if (color1 == 9 || color2 == 9) {
+                return 0;
+            }
             return mixCol;
         }
         else if ((color1 == 9 || color2 == 9)) {
+            // if (color1 == 0 || color2 == 0) {
+            //     return 0;
+            // }
             return 9;
+        }
+        else if ( ([1,2,4].indexOf(color1) > -1 && [3,5,6].indexOf(color2) > -1) || ([1,2,4].indexOf(color2) > -1 && [3,5,6].indexOf(color1) > -1)) {
+            return 7;
         }
         else if (mixCol == 3 || mixCol == 5 || mixCol == 6) {
             return mixCol;
+        }
+        else if (color1 == color2) {
+            return color1;
         }
         else {
             return 7;
