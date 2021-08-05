efd2746a9692cb655523ec2d6ed82d6a6db4b913
CarmenJvR <50364617+CarmenJvR@users.noreply.github.com>
2021-08-05 09:52:29 +0200

    tutorial map done

---
+++ scripts/tutorial.js
@@ -0,0 +1,71 @@
+import { Converter } from "../blockly/converter.js";
+import { Engine } from "../engine/engine.js";
+import { Render } from "../engine/render.js";
+import {  LEVEL_0 } from "../levels/level0.js"
+
+//Vars
+var render;
+
+//Reset
+reset();
+
+//Link buttons
+document.getElementById("start").onclick = function() {  
+    start();
+};
+
+document.getElementById("reset").onclick = function() {  
+    reset();
+};
+
+document.getElementById("preview").onclick = function() {  
+    showSolution();
+};
+
+async function reset() {
+
+    if (!!render) {
+        render.stopRender();
+        await Render.sleep(300);
+        render.resetTo(LEVEL_0);
+
+    } else {
+        render = new Render(document.getElementById("map"), [LEVEL_0]);
+        render.renderFirst();
+    }
+}
+
+async function showSolution() {
+    
+    render.stopRender();
+    let engine = new Engine( LEVEL_0.map, LEVEL_0.player, LEVEL_0.solution);
+    let res = engine.start();
+    let changes = engine.changes;
+
+    render.changes = changes;
+    render.messageState = res;
+    await render.startRender();
+}
+
+
+async function start() {
+
+    let code = Blockly.JavaScript.workspaceToCode(Blockly.getMainWorkspace());
+    // alert(code);
+    let commands = Converter.convert(code);
+    // alert(commands);
+
+    if (commands === null || commands === undefined) {
+        Render.modalAlert("Cannot start an empty solution");
+        return;
+    }
+    
+    render.stopRender();
+    let engine = new Engine( LEVEL_0.map, LEVEL_0.player, commands);
+    let res = engine.start();
+    let changes = engine.changes;
+
+    render.changes = changes;
+    render.messageState = res;
+    await render.startRender();
+}
